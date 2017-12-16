a67eb11755cd25a6887ee1e27948fc4e7ac77008
Luchanskiy Oleg <luchansodev@gmail.com>
2017-12-16 15:41:26 +0300

    Init

---
+++ src/app.js
@@ -0,0 +1,4 @@
+import bot from './bot';
+import * as Invest from './invest';
+
+console.log(Invest.test('123'));
---
+++ src/invest/index.js
@@ -0,0 +1,16 @@
+
+const watchers = {};
+
+/**
+ * Check symbol sell price is greate of value
+ * @param {String} symbol Symbol of stock
+ * @param {Number} value Alarm sell value
+ */
+export const checkStock = (symbol, value) => { };
+
+export const asyncWatch = (id, symbol, value) => { };
+
+export const stopAsyncWatch = (id) => { };
+
+export const test = id => id;
+
