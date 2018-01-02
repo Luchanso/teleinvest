8a2df49f060e943481d076efcbc5014509ce376b
Luchanskiy Oleg <luchansodev@gmail.com>
2018-01-02 22:13:18 +0100

    Refactoring check interval

--- src/app.js
+++ src/app.js
@@ -3,15 +3,14 @@
 import Dotenv from 'dotenv';
 import Telegraf from 'telegraf';
 import Session from 'telegraf/session';
-import Finance from 'yahoo-finance';
 
-import { watchSymbols, watchList } from './db';
 import { start } from './invest/start';
 import { middleware as logMiddleware } from './invest/logger';
 import { help } from './invest/help';
 import { listFunc, listTriggers } from './invest/list';
 import { remove, removeTriggers } from './invest/remove';
 import { get, getTriggers } from './invest/get';
+import { check } from './invest/check';
 import {
   watch,
   triggers as watchTriggers,
@@ -21,8 +20,7 @@ import {
 
 Dotenv.config();
 const bot = new Telegraf(process.env.BOT_TOKEN);
-
-const { log } = console;
+const checkInterval = 5 * 6e4; // 5 min
 
 bot.start(start);
 
@@ -37,44 +35,6 @@ bot.hears(listTriggers, listFunc);
 bot.hears(removeTriggers, remove);
 bot.hears(getTriggers, get);
 
-setInterval(() => {
-  const users = Object.values(watchList);
-  const symbols = Object.keys(watchSymbols);
-
-  if (users.length === 0 || symbols.length === 0) return;
-  log('Update watch list');
-
-  Finance.quote(
-    {
-      symbols,
-      modules: ['price'],
-    },
-    (err, quotes) => {
-      if (err) {
-        log(err);
-        return;
-      }
-
-      Object.keys(quotes).forEach((symbol) => {
-        watchSymbols[symbol] = {
-          ...watchSymbols[symbol],
-          lastPrice: quotes[symbol].price.regularMarketPrice,
-        };
-      });
-
-      users.forEach((user) => {
-        Object.keys(watchList[user]).forEach((symbol) => {
-          const prices = watchList[user][symbol];
-
-          prices.forEach((price) => {
-            if (watchSymbols[symbol].lastPrice > price) {
-              bot.sendMessage(user, `--- ALERT --- \n ${symbol} get price ${symbol}`);
-            }
-          });
-        });
-      });
-    },
-  );
-}, 6e4);
+setInterval(check, checkInterval);
 
 bot.startPolling();
---
+++ src/invest/check/index.js
@@ -0,0 +1,48 @@
+import Finance from 'yahoo-finance';
+import { watchSymbols, watchList } from '../../db';
+
+const { log } = console;
+
+export const check = () => {
+  const users = Object.values(watchList);
+  const symbols = Object.keys(watchSymbols);
+
+  if (users.length === 0 || symbols.length === 0) return;
+  log('Update watch list');
+
+  Finance.quote(
+    {
+      symbols,
+      modules: ['price'],
+    },
+    (err, quotes) => {
+      if (err) {
+        log(err);
+        return;
+      }
+
+      Object.keys(quotes).forEach((symbol) => {
+        watchSymbols[symbol] = {
+          ...watchSymbols[symbol],
+          lastPrice: quotes[symbol].price.regularMarketPrice,
+        };
+      });
+
+      users.forEach((user) => {
+        Object.keys(watchList[user]).forEach((symbol) => {
+          const prices = watchList[user][symbol];
+
+          prices.forEach((price) => {
+            if (watchSymbols[symbol].lastPrice > price) {
+              bot.sendMessage(user, `--- ALERT --- \n ${symbol} get price ${symbol}`);
+            }
+          });
+        });
+      });
+    },
+  );
+}
+
+export default {
+  check,
+};
